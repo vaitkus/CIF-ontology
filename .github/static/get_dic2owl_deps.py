1cd6b2db4f0c4565e2f9513c7a3cea1562737090
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-08-21 14:48:06 +0200

    Ensure `safety` checks the installed dependencies

---
+++ .github/static/get_dic2owl_deps.py
@@ -0,0 +1,48 @@
+"""
+# Retrieve dependencies
+
+Retrieve dependencies from a `requirements.txt`-style file.
+"""
+import argparse
+from pathlib import Path
+import re
+from typing import Set
+
+
+def main(argv: list = None) -> Set[str]:
+    """Retrieve dependencies"""
+    parser = argparse.ArgumentParser(
+        description=main.__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "requirements_files",
+        metavar="FILE",
+        type=Path,
+        nargs="+",
+        help="The path(s) to one or several requirements.txt-style file(s).",
+    )
+    args = parser.parse_args(argv)
+
+    requirements_regex = re.compile(r"^(?P<name>[A-Za-z0-9_-]+)(~=|>=|==).*\n$")
+    dependencies = set()
+    for file in args.requirements_files:
+        if not file.exists():
+            raise FileNotFoundError(f"Could not find {file} !")
+        with open(file.resolve(), "r") as handle:
+            for line in handle.readlines():
+                match = requirements_regex.fullmatch(line)
+                if match is None:
+                    raise ValueError(
+                        f"Couldn't determine package name from line:\n\n  {line}"
+                    )
+                dependencies.add(match.group("name"))
+    return dependencies
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    dependencies = main(argv[1:])
+    grep_extended_regex = f"({'|'.join(dependencies)})"
+    print(grep_extended_regex)
